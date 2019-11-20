d0bb18b5d02e29578ea435c8c43818ef61d887ce
omaima <omemah.sh@gmail.com>
2019-11-20 17:02:32 +0200

    ex second day

---
+++ temp.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"""a,b=1,10
+if a>b:
+    print("a<b")
+elif a<b:
+   print("a<b") 
+else:
+       print("a=b")
+
+a,b=1,10
+max=a if (a>b) else b
+print(max)   
+       
+if 'a' in ['b','c','a']:
+    print("a in the list")
+else : print ("a not in the list")
+
+a=1000
+b=333
+print ("A") if a>b else print ("=") if a==b else print ("B")
+
+a=input("write the number")
+b=input("write the number")
+if a>b:
+  print(a)
+else :
+  print (b) 
+  
+name = input ("write your name") 
+age=int(input ("write your age"))
+if age <=18:
+     print ("under age")
+     school =int(input ("write school average"))
+     if school >= 90:
+          print ("excellent")
+     elif school >= 50 and school < 90:
+          print ("faold")
+if age >=18:
+    print ("adult")        
+    job=input("write your job title")
+    print (age ,name ,job) 
+
+for a in range (10):
+   print("*") 
+
+for a in range (20):
+   print ("*",end "-") 
+   
+   
+     
+      
+for a in range (8):
+   for s in range (a):
+      print("*",end=" ")
+   print( )
+   
+   
+   
+ 
+  for t in range (17):
+      for s in range (17-t):
+          print (' ', end='')
+      for v in  range (t):
+          print ("*",end ="")
+ 
+
+
+q:1          
+a= input("write num")          
+e= input("write num")
+if a>e :
+ print(a)
+else:
+ print (e )  
+
+
+q:2
+s= int(input("enter number:"))
+
+for a in range(10):
+    print (a *s) 
+ 
+    q:3
+
+ 
+for a in range (8):
+   for s in range (a):
+      print("*",end=" ")
+    
+    
+   print( )
+for x in range (7-1):
+   for w in range (6-x):
+     print("*",end=" ")
+   print( )
+  
+ q:4   
+
+letters = ["x","y","z","a","b","c"]
+for i in letters:
+    if i == "a":
+        continue
+    elif i=="c":
+        break;
+    print(i)
+  
+    q:5
+for x in range (12,25,3):
+    print(x)
+ 
+q:6    
+i = 1
+while i<6:
+    print(i)
+    
+    if i==3:
+        break
+    i+=1
+
+
+q:7 
+a= int(input("write num"))
+s=0
+for i in range (0,a+1):
+      s=i+s
+print(s)
+
+q:8
+a= int(input("write num"))
+if i%2==0:
+    print ("even")
+else :
+    print("odd")
+
+ q:9
+for q in range (8):
+    
+for a in range (8):
+   for s in range (a):
+      print("*",end=" ")
+    
+    
+   print( )
+for x in range (7-1):
+   for w in range (6-x):
+     print("*",end=" ")
+   print( )
+ 
+ q:10
+while True:
+try:
+n = input("Please enter an integer: ")
+n = int(n)
+break
+except ValueError:
+print("No valid integer! Please try again ...")
+print("Great, you successfully entered an integer!")
+ 
+q:11
+try :
+    a=3
+    if a<4:
+        b=a/(a-3)
+        print ("value of b= ",b)
+except(ZeroDivisonError,NameError):
+    print("/nError Occured and Handled")
+  """
+  
+for t in range (17):
+      for s in range (17-t):
+          
+          print (' ', end='')
+      for v in  range (t):
+          print ("*",end =" ")
+      print( )
+for q in range (16-1):
+      for w in range (15-q):
+         print (' ', end='')
+      for r in  range (16-q):
+          print ("*",end =" ")
+      print( )
+   
+  
+  
