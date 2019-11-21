d6829fc4ff2b770d34869c956e9f66bbc1efffe4
omaima <omemah.sh@gmail.com>
2019-11-21 13:53:35 +0200

    ex day 3

---
+++ exday3.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 09:51:08 2019
+
+@author: omaima
+"""
+"""
+Q:1
+list=["haya","omaima","yasmin","noor"]
+for c in list: print(c) 
+
+Q:2
+list=[1,2,3,4,5]
+print(sum(list))
+Q:3
+list=[1,2,3,4,5]
+print(max(list))
+
+
+Q:4
+
+a = [10,20,30,20,10,50,60,40,80,50,40]
+print(set(a))
+
+Q:5
+
+a=[1,2]
+if len(a)==0:
+    print("empty list")
+else:
+ print(a) 
+    
+Q:6
+tuple1 = ( 1,20,100,"ssss" )
+for a in tuple1:print(a)
+
+
+Q:7
+num_set = set([0, 1, 2, 3, 4, 5])
+for a in num_set:print(a)
+ 
+ Q:8
+s1={1,2,10,40,5,6}
+s2={2,20,6} 
+print(s1-s2)
+
+
+Q:9
+setx=set(["green","blue"])
+sety=set(["blue","yellow"])
+seta=setx|sety
+print(seta)
+
+
+Q:10
+seta=set([5,10,3,15,2,20])
+print(len(seta))
+
+Q:11
+dic1={1:10,2:20}
+dic2={3:30,42:40}
+dic3={5:50,6:60}
+dic4={}
+for d in (dic1,dic2,dic3):
+    dic4.update(d)
+print(dic4)
+
+Q:12
+
+
+a="hello,world!"
+print(a[1])
+print(a[-5:-2])
+print(len(a))
+print(a.lower())
+print(a.replace("h","j"))
+"""
+
+   
+
